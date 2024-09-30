4448978ec19ca1e9afda3f905d6a1e66ca9ef574
mrindrakumar <indrakumargautam19@gmail.com>
2024-09-30 10:33:28 +0530

    hi
    payment page total amount

--- Controller/paymentgetController.js
+++ Controller/paymentgetController.js
@@ -26,21 +26,25 @@ exports.paymentget=async(req,res)=>{
             console.error("Missing userId in token:", decodedToken);
             return res.status(401).json({ error: true, message: 'Unauthorized: Invalid token' });
           }
-        const payment = await paymentModel.find({customerRef:decodedToken.customerRef});
-        console.log("payment ",payment)
-        if(payment){
-            return res.status(200).json({
-                error: true,
-                message: 'Payment Fetch Successfully',
-                data:[payment]
+          const payment = await paymentModel.find({ customerRef: decodedToken.customerRef });
+
+          if (payment && payment.length > 0) {
+              // Calculate total amount by summing up the 'amount' field
+              const totalAmount = payment.reduce((acc, curr) => acc + Number(curr.amount), 0);
+          
+              return res.status(200).json({
+                  error: false,
+                  message: 'Payments Fetched Successfully',
+                  totalAmount: totalAmount, 
+                  data: payment
               });
-        }else{
-            return res.status(200).json({
-                error: true,
-                message: 'User Not Found',
-                data:[]
-              });  
-        }
+          } else {
+              return res.status(404).json({
+                  error: true,
+                  message: 'No payments found for this customer',
+              });
+          }
+          
 
     }catch(error){
         console.log("error",error.message);
